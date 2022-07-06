75e58ae0a26cedb00aeb256a72001e99421f1d2d
yl <yl.code@qq.com>
2022-07-06 14:48:19 +0800

    add: babel 相关 demo

---
+++ src/w-babel.js
@@ -0,0 +1,7 @@
+import '@babel/polyfill';
+
+const arr = [new Promise(() => {}), new Promise(() => {})];
+
+arr.map((item) => {
+  console.log(item);
+});
---
+++ webpack-babel.config.js
@@ -0,0 +1,49 @@
+const { resolve } = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/w-babel.js',
+  output: {
+    path: resolve(__dirname, './build'), // 存放所生成资源的位置，必须是绝对路径
+    filename: 'js/[name].js', // [name] 表示的是占位符
+  }, // 打包产物的出口
+
+  mode: 'development', //打包的模式 none production development
+
+  plugins: [], // 通过各种 plugin 来增强 webpack 的打包能力
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          // options: , // 可以将配置直接写在 .babelrc 中
+        },
+      },
+    ],
+  }, // 通过 module.rules 来配置不同文件对应使用不同的 loader 进行加载
+};
+
+/**
+ * 打包产物是一个 自执行函数
+ * (function(){
+ *
+ * })({})
+ *
+ * 传给 自执行函数 的参数称为 依赖图谱，也就是一个对象
+ * {"模块的路径": 该模块被打包编译后生成的 chunk }
+ *
+ * chunk: 代码片段, 一个 module 对应一个 chunk
+ *
+ * chunks: chunk 组，表示至少包含一个 chunk
+ *
+ * chunkName: 表示 chunks 的名称，如上面的 entry 为对象时的 key
+ *
+ * bundle: 一个 bundle 对应一个 chunkName
+ *
+ * module: 模块，指的是一个文件
+ *
+ */
